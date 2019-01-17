0644e6d06952a32ab76642695fe62a3a933633ba
Ryan MacIntosh <elec@tronicslab.com>
2019-01-17 16:29:12 -0700

    add Ks0108pi declaration

--- GaragePi.c
+++ GaragePi.c
@@ -7,6 +7,13 @@
 */
 
 #include "GaragePi.h"
+#include "Ks0108pi.h"
+
+Ks0108pi garagePiLCD = {	// then use extern Ks0108pi garagePiLCD;
+	0x40, 0xB8, 0xC0, 		// DISPLAY_SET_Y, DISPLAY_SET_X, DISPLAY_START_LINE, 
+	0x3E, 0x01, 0x00, 		// DISPLAY_ON_CMD, ON, OFF,
+	128, 64 							// SCREEN_WIDTH, SCREEN_HEIGHT
+};
 
 int initGaragePi() {
 	if (!bcm2835_init()){
